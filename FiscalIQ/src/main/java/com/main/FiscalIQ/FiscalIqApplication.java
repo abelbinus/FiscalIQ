34bc297d7b81fa9bbd56e2950d74603a73d1f060
Abel Binu Jacob <56991133+abelbinus@users.noreply.github.com>
2024-04-02 19:17:39 +0100

    Add files via upload

---
+++ FiscalIQ/src/main/java/com/main/FiscalIQ/FiscalIqApplication.java
@@ -0,0 +1,13 @@
+package com.main.FiscalIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscalIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscalIqApplication.class, args);
+	}
+
+}
---
+++ FiscalIQ/src/test/java/com/main/FiscalIQ/FiscalIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.FiscalIQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
